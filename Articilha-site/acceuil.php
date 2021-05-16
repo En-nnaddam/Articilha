1e0dc7475f957adffee4eba936227f05f1ae4037
unknown <abdoennaddam@gmail.com>
2021-05-16 10:49:22 +0100

    changing to php

---
+++ Articilha-site/acceuil.php
@@ -0,0 +1,54 @@
+<section>
+    <div class="container">
+        <div id="container-video">
+            <!-- Video background -->
+            <video id="newspapers-video" autoplay muted loop>
+                <source src="img/newspapers.mp4" type="video/mp4" />
+            </video>
+            <!-- ---------------- -->
+        </div>
+        <div id="video-blend-bg"></div>
+        <!--//todo: La barre de navigation :  -->
+        <!-- Logo -->
+        <main id="header-main">
+            <div id="logo">
+                <img src="img/A-letter-Logo-Design.png" alt="A-letter-Logo-Design" />
+                <p>Articilha</p>
+            </div>
+            <!-- La barre de navigation -->
+            <nav id="navbar">
+                <ul>
+                    <li>
+                        <a href="">
+                            <i class="fas fa-home middle-icons"></i>
+                        </a>
+                    </li>
+                    <li><a href="">Acceuil</a></li>
+                    <li><a href="">Categorie</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">S'inscrire</a></li>
+                    <li><a href="#login">Se connecter</a></li>
+                    <li><a href="">Recherche</a></li>
+                </ul>
+            </nav>
+            <!-- ==================================== -->
+            <i class="fas fa-bars middle-icons" id="menu-icon" onclick="toggle('active-navbar','#navbar')"></i>
+        </main>
+        <!--* Barre de recherche -->
+        <main id="main">
+            <p id="main-text">
+                SUIVEZ EN TEMP REEL TOUTE L'ACTUALITE DES SCIENCES SUR ARTICILHA
+            </p>
+            <form id="form-cherche-bar">
+                <div id="cherche-bar">
+                    <label for="cherche">
+                        <i class="fas fa-search"></i>
+                        <input type="text" name="cherche" id="cherche" placeholder="Cherche" />
+                    </label>
+                </div>
+                <button type="submit">submit</button>
+            </form>
+        </main>
+        <!--* ==================== -->
+    </div>
+</section>
---
+++ Articilha-site/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Articlha</title>
+    <script src="https://kit.fontawesome.com/cd51ce947c.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <!-- ********* Acceuil Section ********* -->
+    <?php include "Acceuil.php"; ?>
+    <!-- ********* Login Section ********* -->
+    <?php include "Login.php"; ?>
+    <!-- ********* Partie Script ********* -->
+    <script src="functions.js"></script>
+</body>
+
+</html>
