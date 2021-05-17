8717318d2542cb03dc0895831d402b8d0414c588
unknown <abdoennaddam@gmail.com>
2021-05-17 02:33:55 +0100

    ^^

--- Articilha-site/login.php
+++ Articilha-site/login.php
@@ -1,6 +1,17 @@
-<section id="login">
-    <div class="conatiner">
-        <!-- <svg
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+</head>
+
+<body>
+    <section id="login">
+        <div class="conatiner">
+            <!-- <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1200 120"
@@ -17,30 +28,33 @@
             d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
           ></path>
         </svg> -->
-        <main>
-            <div class="split">
-                <div id="avatar">
-                    <img src="img/avatar.png" alt="avatar User" />
+            <main>
+                <div class="split">
+                    <div id="avatar">
+                        <img src="img/avatar.png" alt="avatar User" />
+                    </div>
+                    <form action="" class="split">
+                        <div class="split">
+                            <label for="email">
+                                <pre><i class="fas fa-envelope"></i> E-mail : </pre>
+                            </label>
+                            <input type="email" id="email" placeholder="exemple@exemple.xyz" />
+                        </div>
+                        <div class="split">
+                            <label for="password">
+                                <pre><i class="fas fa-key"></i> Mot de passe : </pre>
+                            </label>
+                            <input type="password" id="password" placeholder="Votre mot de passe" />
+                        </div>
+                        <div class="split">
+                            <button type="submit">Login</button>
+                            <a href="#">S'inscrire</a>
+                        </div>
+                    </form>
                 </div>
-                <form action="" class="split">
-                    <div class="split">
-                        <label for="email">
-                            <pre><i class="fas fa-envelope"></i> E-mail : </pre>
-                        </label>
-                        <input type="email" id="email" placeholder="exemple@exemple.xyz" />
-                    </div>
-                    <div class="split">
-                        <label for="password">
-                            <pre><i class="fas fa-key"></i> Mot de passe : </pre>
-                        </label>
-                        <input type="password" id="password" placeholder="Votre mot de passe" />
-                    </div>
-                    <div class="split">
-                        <button type="submit">Login</button>
-                        <a href="#">S'inscrire</a>
-                    </div>
-                </form>
-            </div>
-        </main>
-    </div>
-</section>
+            </main>
+        </div>
+    </section>
+</body>
+
+</html>
